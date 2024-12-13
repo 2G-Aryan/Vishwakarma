177f7d3911371d62287fdcb8207aa038ea9e3e92
2G-Aryan <aryanupadhyay771@gmail.com>
2024-12-13 15:31:58 +0530

    Update HeroSection.jsx

--- components/ui/HeroSection.jsx
+++ components/ui/HeroSection.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button } from "./Button";
+import { Button } from "./button";
 import { TbArrowUpRight } from 'react-icons/tb';
 import Image from "next/image";
 import { titleVariants, desVariants, tagVariants } from '@/utils/animation';
